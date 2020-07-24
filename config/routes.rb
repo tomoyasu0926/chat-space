e6d34d8fd0d35301afa00d7a3e25e8d27c53408e
tomoyasu0926 <67819156+tomoyasu0926@users.noreply.github.com>
2020-07-24 18:25:19 +0900

    Revert "グループ機能"

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "messages#index"
-  root 'groups#index'
   resources :users, only: [:edit, :update]
-  resources :groups, only: [:new, :create]
-  resources :groups, only: [:new, :create, :edit, :update]
-  resources :groups, only: [:index, :new, :create, :edit, :update]
+
 end
